f30494f87178df9ad99ea697c4305c5812315f2a
Victor Lee <vic@Victors-MacBook-Pro.local>
2017-11-09 14:21:02 -1000

    adds ex5

--- solutions.js
+++ solutions.js
@@ -105,6 +105,15 @@ console.log(plainBox);
             - add a key named `passengers` and set it to be an empty array.
 */
 
+var stockCar = {
+  model: "Tesla",
+  year: 2016,
+  automaticTransmission: true,
+  driver: null,
+  passengers: []
+};
+
+console.log(stockCar);
 
 /*
 
