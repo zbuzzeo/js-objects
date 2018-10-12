120b0a4a415d667a017d19534ac2f4a0f77f5098
unknown <zeke.buzzeo@gmail.com>
2018-10-11 19:00:11 -1000

    Completed ex7

--- exercises.js
+++ exercises.js
@@ -181,6 +181,68 @@ console.log(plainPerson['age']);
             ...
  */
 
+var arrayOfObjects = [
+      {
+        id: 0,
+        date: "Monday Jan 25 2015 2:01 PM",
+        total: "279.38"
+      },
+      {
+        id: 1,
+        date: "Monday Jan 27 2015 11:31 AM",
+        total: "79.80"
+      },
+      {
+        id: 2,
+        date: "Monday Feb 1 2015 7:56 AM",
+        total: "15.62"
+      },
+      {
+        id: 3,
+        date: "Monday Feb 1 2015 9:43 AM",
+        total: "19.83"
+      },
+      {
+        id: 4,
+        date: "Monday Feb 1 2015 11:08 PM",
+        total: "56.69"
+      },
+      {
+        id: 5,
+        date: "Monday Feb 13 2015 10:22 AM",
+        total: "137.92"
+      },
+      {
+        id: 6,
+        date: "Monday Feb 14 2015 6:54 PM",
+        total: "938.65"
+      },
+      {
+        id: 7,
+        date: "Monday Feb 14 2015 7:17 PM",
+        total: "43.77"
+      },
+      {
+        id: 8,
+        date: "Monday Feb 14 2015 7:18 PM",
+        total: "28.54"
+      },
+      {
+        id: 9,
+        date: "Monday Feb 14 2015 7:18 PM",
+        total: "194.33"
+      }
+]
+
+function printOrders(listOrders){
+      for (var i = 0; i < listOrders.length; i++){
+            var divider = '===============';
+            console.log(divider, '\nid:', listOrders[i].id, '\npurchase date', listOrders[i].date, '\npurchase total: $',  listOrders[i].total);
+      }
+}
+
+spacer(7);
+printOrders(arrayOfObjects);
 
 /*
 8. Addition with an object
